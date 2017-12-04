c9a2f6f4ef8ae7675b4180662c55446053b4085e
wellfrog <wellfrog16@gmail.com>
2017-12-05 02:38:02 +0800

    更新

--- src/components/XHeader.vue
+++ src/components/XHeader.vue
@@ -1,33 +1,33 @@
 <template>
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-nav" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Shop</a>
-    </div>
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-nav" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Shop</a>
+        </div>
 
-    <div class="collapse navbar-collapse" id="top-nav">
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">站位看看</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">frog <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">登陆</a></li>
-            <li><a href="#">状态</a></li>
-            <li><a href="#">状态</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">购物车</a></li>
-          </ul>
-        </li>
-      </ul>
+        <div class="collapse navbar-collapse" id="top-nav">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">站位看看</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">frog <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">登陆</a></li>
+                        <li><a href="#">状态</a></li>
+                        <li><a href="#">状态</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">购物车</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </div>
-  </div>
 </nav>
 </template>
 
--- src/views/List.vue
+++ src/views/List.vue
@@ -1,46 +1,34 @@
 <template>
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-xs-3">
-            <div class="panel panel-info">
-                <div class="panel-heading">价格过滤</div>
-                <div class="panel-body list-group">
-                    <a v-for="item in idFiler" :key="item.id" class="list-group-item" href="#">
-                        {{ item.start }} - {{ item.end }}
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class="col-xs-9">
-            <div class="row">
-                <div class="col-xs-12 navbar navbar-default">
-                    <div class="container-fluid">
-                        <ul class="nav navbar-nav navbar-right">
-                            <li><a href="#">测试</a></li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">价格 <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li v-for="item in idFiler" :key="item.id">
-                                        <a href="#">{{ item.start }} - {{ item.end }}</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">排序 <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">默认</a></li>
-                                    <li><a href="#">价格</a></li>
-                                </ul>
+<div class="row">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 navbar navbar-default">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">测试</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">价格 <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li v-for="item in idFiler" :key="item.id">
+                                <a href="#">{{ item.start }} - {{ item.end }}</a>
                             </li>
                         </ul>
-                    </div>
-                </div>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">排序 <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">默认</a></li>
+                            <li><a href="#">价格</a></li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
-            <div class="row list-goods">
-                <div v-for="item in goodsList" :key="item.id" class="col-sm-3 col-xs-12">
-                    <img v-bind:src="item.url" alt="" class="img-responsive">
-                    <p>{{ item.title }}</p>
-                </div>
+        </div>
+
+
+        <div class="row list-goods">
+            <div v-for="item in goodsList" :key="item.id" class="col-sm-3 col-xs-12">
+                <img v-bind:src="item.url" alt="" class="img-responsive">
+                <p>{{ item.title }}</p>
             </div>
         </div>
     </div>
@@ -80,9 +68,11 @@
 </script>
 
 <style lang="less">
+@media (min-width: 768px) {
     .list-goods {
         p {
             height: 100px;
         }
     }
+}
 </style>
